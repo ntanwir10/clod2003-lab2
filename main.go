db85333569e174506622e4ba8b7f621d3685ba7e
Oviya-krishnamoorthy <oviya1527@gmail.com>
2024-09-27 16:02:09 -0400

    Calculating Area of a Circle

--- main.go
+++ main.go
@@ -4,4 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to Group B's Week 4 Project!")
+
+	radius := 7.0
+	area := CircleArea(radius)
+	fmt.Printf("The area of the circle with radius %.2f is %.2f\n", radius, area)
+
 }
